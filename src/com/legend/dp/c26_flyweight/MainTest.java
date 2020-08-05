570349e2bb6a390b292981f8125178d26cf76d7b
Robin Wang <510471194@qq.com>
2020-08-06 00:40:47 +0800

    add annotation for flyweight

--- src/com/legend/dp/c26_flyweight/MainTest.java
+++ src/com/legend/dp/c26_flyweight/MainTest.java
@@ -1,5 +1,12 @@
 package com.legend.dp.c26_flyweight;
 
+/*
+ * 当前示例：享元模式-->快速生成类似网站
+ * 定义：运用共享技术有效支持大量细粒度对象
+ * 适用场景：一个应用程序因为使用了大量对象而造成了很大存储开销或者大量对象的某些状态可以作为外部状态，剩下的状态可以用一个共享对象代替
+ * 特点：可以避免使用大量相似类，大幅减少实例个数，减少资源消耗
+ * */
+
 public class MainTest {
 	
 	public static void main(String[] args) {
